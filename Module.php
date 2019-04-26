72352dd216ac4c0c5a4531218485f8758ac0b17a
nadine-shi <nadine@afterlogic.com>
2019-04-26 14:30:27 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -8,6 +8,8 @@
 namespace Aurora\Modules\DemoModePlugin;
 
 /**
+ * Makes restriction of access to some functionality for demo users.
+ * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
