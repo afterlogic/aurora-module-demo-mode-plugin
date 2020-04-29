4108bac77cc1ddbcd01cdd80da7c42a5ae29cdeb
sash04ek <a.ovcharov@gmail.com>
2020-04-29 10:26:39 +0300

    Added AdvancedMode config parameter

--- Module.php
+++ Module.php
@@ -9,7 +9,7 @@ namespace Aurora\Modules\DemoModePlugin;
 
 /**
  * Makes restriction of access to some functionality for demo users.
- * 
+ *
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
@@ -19,11 +19,11 @@ namespace Aurora\Modules\DemoModePlugin;
 class Module extends \Aurora\System\Module\AbstractModule
 {
 	protected $bDemoUser = false;
-	
+
 	protected $bNewDemoUser = false;
-	
+
 	/***** private functions *****/
-	
+
 	protected function checkDemoUser($sPublicId)
 	{
 		$sDemoLogin = $this->getConfig('DemoLogin', '');
@@ -34,20 +34,20 @@ class Module extends \Aurora\System\Module\AbstractModule
 		return ($sCurrentDomain === $sDemoDomain);
 	}
 
-	public function init() 
+	public function init()
 	{
 		$this->subscribeEvent('Core::Login::before', array($this, 'onBeforeLogin'), 10);
 		$this->subscribeEvent('Core::Login::after', array($this, 'onAfterLogin'), 10);
 		$this->subscribeEvent('Core::GetDigestHash::after', array($this, 'onAfterGetDigestHash'), 10);
-		
+
 		$oUser = \Aurora\System\Api::getAuthenticatedUser();
 		if ($oUser instanceof \Aurora\Modules\Core\Classes\User)
 		{
 //			$aMatches = array();
 //			preg_match('/demo\d*@.+/', $oUser->PublicId, $aMatches, PREG_OFFSET_CAPTURE);
-			
+
 			$sDemoLogin = $this->getConfig('DemoLogin', '');
-			
+
 			$sCurrentDomain = preg_match("/.+@(localhost|.+\..+)/", $oUser->PublicId, $matches) && isset($matches[1]) ? $matches[1] : '';
 			$sDemoDomain = preg_match("/.+@(localhost|.+\..+)/", $sDemoLogin, $matches) && isset($matches[1]) ? $matches[1] : '';
 
@@ -62,53 +62,65 @@ class Module extends \Aurora\System\Module\AbstractModule
 			}
 		}
 	}
-	
+
 	public function onBeforeForbiddenAction(&$aArgs, &$mResult)
 	{
 		throw new \Aurora\System\Exceptions\ApiException(\Aurora\System\Notifications::DemoAccount);
 	}
-	
+
 	public function onBeforeLogin(&$aArgs, &$mResult)
 	{
 		$DemoUserType = $this->getConfig('DemoUserType', '');
 		$sDemoLogin = $this->getConfig('DemoLogin', '');
-		
-		if ($sDemoLogin === $aArgs['Login'])
-		{
-			switch ($DemoUserType)
-			{
-				case \Aurora\Modules\DemoModePlugin\Enums\DemoUserType::Mail:
-					$userCredentials = $this->createMailbox();
-					break;
-				case \Aurora\Modules\DemoModePlugin\Enums\DemoUserType::Db:
-					$userCredentials = $this->createDbUser();
-					break;
-			}
 
-			if (!empty($userCredentials)) {
-				$aArgs['Login'] = $userCredentials['login'];
-				$aArgs['Password'] = $userCredentials['password'];
-				$aArgs['NewDemoUser'] = true;
-				$this->bNewDemoUser = true;
-			}
-		}
-		else
+		$bAdvancedMode = $this->getConfig('AdvancedMode', true);
+		if ($bAdvancedMode) # Advanced mode
 		{
-			$aLogin = explode('@', $aArgs['Login']);
-			$aDemoLogin = explode('@', $sDemoLogin);
-			if (isset($aLogin[1], $aDemoLogin[1]))
+			if ($sDemoLogin === $aArgs['Login'])
 			{
-				$sDomain = $aLogin[1];
-				$sDemoDomain = $aDemoLogin[1];
-				if ($sDomain === $sDemoDomain && $aArgs['Password'] === 'demo')
+				switch ($DemoUserType)
 				{
-					$sDemoRealPass = $this->getConfig('DemoRealPass', '');
-					$aArgs['Password'] = $sDemoRealPass;
+					case \Aurora\Modules\DemoModePlugin\Enums\DemoUserType::Mail:
+						$userCredentials = $this->createMailbox();
+						break;
+					case \Aurora\Modules\DemoModePlugin\Enums\DemoUserType::Db:
+						$userCredentials = $this->createDbUser();
+						break;
+				}
+
+				if (!empty($userCredentials))
+				{
+					$aArgs['Login'] = $userCredentials['login'];
+					$aArgs['Password'] = $userCredentials['password'];
+					$aArgs['NewDemoUser'] = true;
+					$this->bNewDemoUser = true;
+				}
+			}
+			else
+			{
+				$aLogin = explode('@', $aArgs['Login']);
+				$aDemoLogin = explode('@', $sDemoLogin);
+				if (isset($aLogin[1], $aDemoLogin[1]))
+				{
+					$sDomain = $aLogin[1];
+					$sDemoDomain = $aDemoLogin[1];
+					if ($sDomain === $sDemoDomain && $aArgs['Password'] === 'demo')
+					{
+						$sDemoRealPass = $this->getConfig('DemoRealPass', '');
+						$aArgs['Password'] = $sDemoRealPass;
+					}
 				}
 			}
 		}
+		else # Simple mode
+		{
+			if ($sDemoLogin === $aArgs['Login'])
+			{
+				$aArgs['Password'] = $this->getConfig('DemoRealPass', '');
+			}
+		}
 	}
-	
+
 	protected function createMailbox()
 	{
 		$result = null;
@@ -116,27 +128,27 @@ class Module extends \Aurora\System\Module\AbstractModule
 		$sDemoRealPass = $this->getConfig('DemoRealPass', '');
 		$sApiUrl = $this->getConfig('ApiUrl', '');
 		$sNewUserLogin = '';
-		
+
 		if ($sDemoLogin && $sApiUrl !== '')
 		{
 			$sDomain = preg_match("/.+@(localhost|.+\..+)/", $sDemoLogin, $matches) && isset($matches[1]) ? $matches[1] : '';
-		
+
 			$sNewUserLogin = @file_get_contents($sApiUrl.$sDomain);
 
-			if ($sNewUserLogin) 
+			if ($sNewUserLogin)
 			{
 				$sEmail = $sNewUserLogin."@".$sDomain;
-				
+
 				$result = array(
 					'login' => $sEmail,
 					'password' => $sDemoRealPass
 				);
 			}
 		}
-		
+
 		return $result;
 	}
-	
+
 	protected function createDbUser()
 	{
 		$result = null;
@@ -147,7 +159,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 
 		$sLogin = 'user-'.base_convert(substr(str_pad(microtime(true)*100, 15, '0'), -11, 8), 10, 32).'@'.$sDomain;
 		$sPassword = !empty($sDemoRealPass) ? $sDemoRealPass : substr(str_shuffle('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890___---%%%$$$&&&'), 0, 20);
-		
+
 		\Aurora\Api::skipCheckUserRole(true);
 
 		$oSettings =&\Aurora\System\Api::GetSettings();
@@ -176,7 +188,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 		}
 
 		\Aurora\Api::skipCheckUserRole(false);
-		
+
 		if (isset($dbAccont) && isset($dbAccont['EntityId']))
 		{
 			$result = array(
@@ -184,10 +196,10 @@ class Module extends \Aurora\System\Module\AbstractModule
 				'password' => $sPassword
 			);
 		}
-			
+
 		return $result;
 	}
-	
+
 	public function onAfterLogin(&$aArgs, &$mResult)
 	{
 		if ($this->bNewDemoUser)
@@ -196,7 +208,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 			$this->populateContacts($aArgs);
 		}
 	}
-	
+
 	public function onAfterGetDigestHash(&$aArgs, &$mResult)
 	{
 		if ($this->checkDemoUser($aArgs['Login']))
@@ -208,22 +220,22 @@ class Module extends \Aurora\System\Module\AbstractModule
 	protected function populateContacts($aArgs)
 	{
 		$oContactsDecorator = \Aurora\Modules\Contacts\Module::Decorator();
-		
+
 		if ($oContactsDecorator)
 		{
 			$oGroupResult = $oContactsDecorator->CreateGroup(array(
 				'Name' => 'Afterlogic Support Team'
 			));
-			
+
 			$aContactData = $this->getConfig('SampleContactData');
-			
+
 			if (!empty($aContactData))
 			{
 				if (isset($aContactData['PrimaryEmail']) && !is_numeric($aContactData['PrimaryEmail']))
 				{
 					$aContactData['PrimaryEmail'] = constant($aContactData['PrimaryEmail']);
 				}
-		
+
 				if (is_array($aContactData))
 				{
 					if ($oGroupResult)
@@ -234,7 +246,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 					$oContactsDecorator->CreateContact($aContactData);
 				}
 			}
-			
+
 			//Import of .vcf doesn't work properly because file should be uploaded to the server. It's impossible to import existing file.
 //			$sampleContactPath = dirname(__FILE__).'\data\contact.vcf';
 //			$sSampleContact = @file_get_contents(dirname(__FILE__).'\data\contact.vcf');
@@ -252,12 +264,12 @@ class Module extends \Aurora\System\Module\AbstractModule
 //			}
 		}
 	}
-	
+
 	protected function populateInbox($aArgs)
 	{
 		$sResult = false;
 		$result = preg_match("/(.+)@(?:localhost|.+\..+)/", $aArgs['Login'], $matches);
-		
+
 		if ($result && isset($matches[1]))
 		{
 			$sUserLogin = $matches[1];
@@ -269,25 +281,25 @@ class Module extends \Aurora\System\Module\AbstractModule
 				$sResult = trim(shell_exec($sPostProcessScript. ' ' . $sPostProcessType . ' ' . $sUserLogin));
 			}
 		}
-		
+
 		return $sResult;
 	}
-	
+
 	/***** private functions *****/
-	
+
 	/***** public functions might be called with web API *****/
-	
+
 	public function IsDemoUser()
 	{
 		return $this->bDemoUser || $this->bNewDemoUser;
 	}
-	
+
 	public function GetSettings()
 	{
 		return array(
 			'IsDemoUser' => $this->IsDemoUser()
 		);
 	}
-	
+
 	/***** public functions might be called with web API *****/
 }
