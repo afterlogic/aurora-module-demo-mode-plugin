91c7036fa087cea584ba4c4aef8019db3781fce7
Vasil Sokolov <vasil@afterlogic.com>
2023-04-18 11:19:39 +0200

    Refactored module settings

---
+++ Settings.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * This code is licensed under AGPLv3 license or Afterlogic Software License
+ * if commercial version of the product was purchased.
+ * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
+ */
+
+namespace Aurora\Modules\DemoModePlugin;
+
+use Aurora\System\SettingsProperty;
+use Aurora\Modules\DemoModePlugin\Enums;
+
+/**
+ * @property bool $Disabled
+ * @property Enums\DemoUserType $DemoUserType
+ * @property string $DemoLogin
+ * @property string $DemoRealPass
+ * @property string $ApiUrl
+ * @property string $PostProcessScript
+ * @property string $PostProcessType
+ * @property array $SampleContactData
+ * @property bool $AdvancedMode
+ */
+
+class Settings extends \Aurora\System\Module\Settings
+{
+    protected function initDefaults()
+    {
+        $this->aContainer = [
+            "Disabled" => new SettingsProperty(
+                false,
+                "bool",
+                null,
+                "",
+            ),
+            "DemoUserType" => new SettingsProperty(
+                "Mail",
+                "spec",
+                Enums\DemoUserType::class,
+                "",
+            ),
+            "DemoLogin" => new SettingsProperty(
+                "",
+                "string",
+                null,
+                "",
+            ),
+            "DemoRealPass" => new SettingsProperty(
+                "",
+                "string",
+                null,
+                "",
+            ),
+            "ApiUrl" => new SettingsProperty(
+                "",
+                "string",
+                null,
+                "",
+            ),
+            "PostProcessScript" => new SettingsProperty(
+                "",
+                "string",
+                null,
+                "",
+            ),
+            "PostProcessType" => new SettingsProperty(
+                "",
+                "string",
+                null,
+                "",
+            ),
+            "SampleContactData" => new SettingsProperty(
+                "",
+                "array",
+                null,
+                "",
+            ),
+            "AdvancedMode" => new SettingsProperty(
+                true,
+                "bool",
+                null,
+                "",
+            ),
+        ];
+    }
+}
